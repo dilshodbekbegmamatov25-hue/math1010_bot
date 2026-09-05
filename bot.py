8e6c2f5c062ff00797f923f80f8c6e08efeeb7b2
dilshodbekbegmamatov25-hue <dilshodbekbegmamatov25@gmail.com>
2026-09-05 08:48:18 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,12 @@ import telebot
 from telebot.types import WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 import json
 
-TOKEN = "8978768448:AAEhkeTQMTktCnq8K6yWAQIBtw1QQ1zp8YY"
+# Tokeningizni yozing
+TOKEN = "8978768448:AAEhkeTQMTktCnq8K6yWAQIBtw1QQ1zp8YY
+"
 bot = telebot.TeleBot(TOKEN)
 
+# GitHub Pages havolangiz
 WEB_APP_URL = "https://dilshodbekbegmamatov25-hue.github.io/math1010_bot/"
 
 @bot.message_handler(commands=['start'])
@@ -22,7 +25,7 @@ def send_welcome(message):
         reply_markup=markup
     )
 
-# Render orqali keladigan natijalarni qabul qilish uchun server
+# Render port talabini qondirish va natijani qabul qilish uchun server
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -40,9 +43,17 @@ class SimpleHandler(BaseHTTPRequestHandler):
                 score = data.get('score')
                 
                 if user_id:
+                    level_text = "Boshlang'ich"
+                    if score > 7:
+                        level_text = "Yuqori / Professional! 🏆"
+                    elif score > 4:
+                        level_text = "O'rta"
+
                     bot.send_message(
                         user_id, 
-                        f"📊 **Test natijasi:**\n\n👤 Ism: {name}\n🎯 Ball: {score} / 10", 
+                        f"🎉 **Test yakunlandi!**\n\n"
+                        f"📊 Sizning natijangiz: 10 ta savoldan {score} ta to'g'ri\n"
+                        f"💡 Darajangiz: 🥉 {level_text}", 
                         parse_mode="Markdown"
                     )
                     self.send_response(200)
